178cdd9281d5150124013a1f78617861a1c6c3e8
Tristan Perera <tristan.e.perera@gmail.com>
2019-01-11 18:37:16 -0800

    init- first push with much of app done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/layout/Header'
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Finish React To-Do List App',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Complete one algo on the whiteboard',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Start creating a React Native app',
+        completed: false
+      }
+    ]
+  }
+
+  // Toggle Complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if (todo.id === id){
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    }) })
+  }
+  
+
+  // Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+
+      </div>
+
+      <Header />
+      <AddTodo />
+      <Todos todos={this.state.todos} markComplete={this.markComplete}  delTodo={this.delTodo}/>
+      
       </div>
     );
   }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+
+export class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      color: 'darkgrey',
+      backgroundColor: 'lightgrey',
+      borderBottom: '1px darkgrey solid',
+      padding: '5px 10px',
+      fontFamily: 'Arial',
+      textDecoration: this.props.todo.completed ?
+      'Line-through' : 'none'
+    }
+  }
+  
+  render() {
+    const {id, title} = this.props.todo;
+    return (
+      <div style={this.getStyle()}>
+        <p>
+          <input type='checkbox' onChange={this.props.markComplete.bind(this, id)} /> {' '}
+          { title }
+          <button style={btnStyle} onClick={this.props.delTodo.bind(this, id)} >x</button>
+        </p>
+      </div>
+    )
+  }
+}
+
+// PropTypes
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+}
+
+const btnStyle = {
+  background: '#ff0000',
+  color: '#fff',
+  border: 'none',
+  padding: '1px 5.5px',
+  borderRadius: "50%",
+  curser: 'pointer',
+  float: 'right'
+}
+
+
+export default TodoItem
